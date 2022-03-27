e65b27fff8e1b35fd9e21785761d692c7a640f77
Patrick Marsceill <pmarsceill@users.noreply.github.com>
2022-03-27 20:03:45 +0000

    add realtime rendering

--- app/components/tab/music-render.client.tsx
+++ app/components/tab/music-render.client.tsx
@@ -1,39 +1,47 @@
-import { createElement, RefObject, useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 // import Vex from 'vexflow'
 import { Vex, Artist, VexTab } from '../../../vendor/vextab'
 
 type Props = {
   value: string
-  editor: RefObject<HTMLTextAreaElement>
 }
 
-const MusicStaff = ({ value, editor }: Props) => {
+const MusicStaff = ({ value }: Props) => {
   const canvasRef = useRef<HTMLDivElement>(null)
-  // const Renderer = Vex.Flow.Renderer()
+  const errorMessageRef = useRef<HTMLDivElement>(null)
+
+  const [artist, setArtist] = useState<Artist>(
+    new Artist(10, 30, 800, { scale: 1, bottom_spacing: 8 })
+  )
+  const [tab, setTab] = useState<VexTab>(new VexTab(artist))
+  const [renderer, setRenderer] = useState<Vex.IRenderer | null>(null)
 
   useEffect(() => {
     const VF = Vex.Flow
     const musicCanvas = canvasRef.current
-
-    const renderer = new VF.Renderer(musicCanvas, VF.Renderer.Backends.SVG)
-
-    console.log('---> renderer', renderer)
-    const artist = new Artist(10, 30, 800, { scale: 0.7, bottom_spacing: 0 })
-    const tab = new VexTab(artist)
-
-    try {
-      tab.parse('tabstave notation=false\n')
-      artist.render(renderer)
-    } catch (e) {
-      console.log('---> error', e)
-    }
-
-    console.log('----> content', value)
-    console.log('----> Editor content', editor.current.value)
-    console.log('----> canvasRef', canvasRef.current)
+    setRenderer(new VF.Renderer(musicCanvas, VF.Renderer.Backends.SVG))
   }, [])
 
-  return <div ref={canvasRef} id="canvas"></div>
+  useEffect(() => {
+    if (canvasRef.current && renderer) {
+      try {
+        artist.reset()
+        setTab(new VexTab(artist))
+
+        tab.parse(value)
+        artist.render(renderer)
+      } catch (e) {
+        console.log('----> error', e)
+      }
+    }
+  }, [value])
+
+  return (
+    <>
+      <div ref={canvasRef} id="canvas"></div>
+      <div ref={errorMessageRef} />
+    </>
+  )
 }
 
 export default MusicStaff
--- app/routes/tabs/$id.tsx
+++ app/routes/tabs/$id.tsx
@@ -1,4 +1,10 @@
-import { json, MetaFunction, useLoaderData } from 'remix'
+import {
+  json,
+  MetaFunction,
+  useLoaderData,
+  useFetcher,
+  ActionFunction,
+} from 'remix'
 import type { LoaderFunction } from 'remix'
 
 import { db } from '~/db.server'
@@ -28,10 +34,41 @@ export const meta: MetaFunction = (page) => {
   }
 }
 
+export const action: ActionFunction = async ({ request, params }) => {
+  const form = await request.formData()
+  const intId = Number(params.id)
+  const newContent = form.get('content')?.toString()
+  // // we do this type check to be extra sure and to make TypeScript happy
+  // // we'll explore validation next!
+  // console.log('---> new content', newContent)
+
+  // if (typeof newContent !== 'string') {
+  //   console.log('---> error content')
+  //   console.error('error loading content')
+  //   // throw new Error(`Form not submitted correctly.`)
+  // }
+
+  console.log('---> Update tab', intId, newContent)
+  await db.tab.update({
+    where: { id: intId },
+    data: { content: newContent },
+  })
+}
+
 export default function Tab() {
   const tab = useLoaderData()
+  const fetcher = useFetcher()
+
   const [mounted, setMounted] = useState(false)
+  const [tabValue, setTabValue] = useState(tab.content)
   const editorRef = useRef<HTMLTextAreaElement>(null)
+
+  const handleChange = (e) => {
+    const content = e.currentTarget.value
+    console.log('---> content', content)
+    setTabValue(content)
+    fetcher.submit({ content: content }, { method: 'post' })
+  }
 
   useEffect(() => {
     setMounted(true)
@@ -46,15 +83,17 @@ export default function Tab() {
           <span key={artist.artistId}> {artist.artist.name}</span>
         ))}
       </p>
-
-      <textarea
-        spellCheck="false"
-        autoCapitalize="false"
-        id="editor"
-        defaultValue={tab.content}
-        ref={editorRef}
-      />
-      {mounted ? <MusicStaff value={tab.content} editor={editorRef} /> : null}
+      <fetcher.Form>
+        <textarea
+          name="tabContent"
+          spellCheck="false"
+          autoCapitalize="false"
+          defaultValue={tabValue}
+          onChange={handleChange}
+          ref={editorRef}
+        />
+      </fetcher.Form>
+      {mounted ? <MusicStaff value={tabValue} /> : null}
     </main>
   )
 }
